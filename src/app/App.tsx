47e129b4cbf2d90c943d9e88bec08f5c16f4f08b
neryad <dayern.gomez@hotmail.com>
2026-03-30 15:52:48 -0400

    add natiwind

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { AppSessionProvider } from './providers/AppSessionProvider';
-import { RootNavigator } from './navigation/RootNavigator';
-
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { AppSessionProvider } from "./providers/AppSessionProvider";
+import { RootNavigator } from "./navigation/RootNavigator";
+import "./global.css";
 
 export default function App() {
   return (
